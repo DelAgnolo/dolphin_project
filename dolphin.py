a90f779b01f5a48069965763998dc2f48a5ebcfc
Adrien Hellec <adrien.hellec@epita.fr>
2019-11-23 17:46:50 +0100

    flatten quantities to get correct nav : 2,39 ratio

--- dolphin.py
+++ dolphin.py
@@ -3,39 +3,54 @@ from functools import reduce
 
 from api_logic import APILogic
 
+TOTAL_ASSETS = 15
+
+
 def get_weight(ratios):
-  return (2*ratios.sharpe*ratios.ret)/ratios.vol
+    return (2*ratios.sharpe*ratios.ret)/ratios.vol
+
 
 def remove_negatives(assets):
-  new_assets = []
-  for item in assets:
-    if item.ratios.sharpe >= 0.5 and \
-      item.ratios.vol >= 0 and \
-      item.ratios.ret >= 2:
-      new_assets.append(item)
+    new_assets = []
+    for item in assets:
+        if item.ratios.sharpe >= 0.5 and \
+                item.ratios.vol >= 0 and \
+                item.ratios.ret >= 2:
+            new_assets.append(item)
 
-  return new_assets
+    return new_assets
 
 
 def main():
-  app = APILogic()
-  assets = app.get_assets()
-  filtered = remove_negatives(assets)
-  sorted_assets = sorted(filtered, key=lambda x: get_weight(x.ratios), reverse=True)
-  best_assets = sorted_assets[:15]
+    app = APILogic()
+    assets = app.get_assets()
+    filtered = remove_negatives(assets)
+    sorted_assets = sorted(
+        filtered, key=lambda x: get_weight(x.ratios), reverse=True)
+    best_assets = sorted_assets[:TOTAL_ASSETS]
 
-  app.get_assets_nav(best_assets)
+    app.get_assets_nav(best_assets)
 
-  total_nav = reduce(lambda a, b: a + b.nav, best_assets, 0)
+    total_nav = int(reduce(lambda a, b: a + b.nav, best_assets, 0))
+    print("total nav : " + str(total_nav))
 
-  total_quantity = 0
-  for item in best_assets:
-    item.quantity = int((total_nav/15)*get_weight(item.ratios))
-    total_quantity += item.quantity
+    total_nav2 = 0
+    for item in best_assets:
+        item.quantity = int(((total_nav)/TOTAL_ASSETS/item.nav)*1000)
+        total_nav2 += item.nav*item.quantity
 
-  app.update_ptf(best_assets)
+    # app.update_ptf(best_assets)
 
-  print("new ratio :" + app.get_user_ptf_sharpe())
+    print("new ratio :" + app.get_user_ptf_sharpe())
+
+
+def check():
+    app = APILogic()
+    ptf = app.get_user_ptf()
+    for item in ptf.assets:
+        print(item.id + " - " + str(item.quantity))
+
+    print("ratio :" + app.get_user_ptf_sharpe())
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,24 +1,26 @@
 class Asset:
-  def __init__(self, id, is_portfolio, nav=None, ratios=None, quantity=None):
-    self.id = id
-    self.is_portfolio = is_portfolio
-    self.nav = nav
-    self.quantity = quantity
-    self.ratios = ratios
+    def __init__(self, id, is_portfolio, type=None, nav=None, ratios=None, quantity=None):
+        self.id = id
+        self.is_portfolio = is_portfolio
+        self.type = type
+        self.nav = nav
+        self.quantity = quantity
+        self.ratios = ratios
+
 
 class AssetRatio:
-  def __init__(self, asset_id, sharpe=None, vol=None, ret=None):
-    self.asset_id = asset_id
-    self.sharpe = sharpe
-    self.vol = vol
-    self.ret = ret
+    def __init__(self, asset_id, sharpe=None, vol=None, ret=None):
+        self.asset_id = asset_id
+        self.sharpe = sharpe
+        self.vol = vol
+        self.ret = ret
 
 
 class Portfolio:
-  def __init__(self, id, sharpe, assets=None):
-    self.id = id
-    self.sharpe = sharpe
-    if assets is None:
-      self.assets = []
-    else:
-      self.assets = assets
+    def __init__(self, id, label, sharpe=None, assets=None):
+        self.id = id
+        self.sharpe = sharpe
+        if assets is None:
+            self.assets = []
+        else:
+            self.assets = assets
